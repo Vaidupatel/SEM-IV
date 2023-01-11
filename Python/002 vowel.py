9e929354c7f0f348ff431a6c4839c73b5d9213c4
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2023-01-11 19:44:17 +0530

    Create 002 vowel.py

---
+++ Python/002 vowel.py
@@ -0,0 +1,5 @@
+x=input('Enter the character to chack wther it\'s vovel or not ')
+if(x=='a' or x=='A' or x=='e' or x=='E'or x=='i' or x=='I' or x=='o' or x=='O' or x=='u' or x=='U'):
+    print('The character is vowel ( ͡° ͜ʖ ͡°) ')
+else:
+    print('The charecter is not vowel  ¯\_(ツ)_/¯')

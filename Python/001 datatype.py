5e6821c231a4ef50d00274e6f692bea449f66d2f
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2023-01-11 19:31:46 +0530

    Create 001 datatype.py

---
+++ Python/001 datatype.py
@@ -0,0 +1,19 @@
+a=10
+b=12.4
+c="vaid"
+d=False
+e=('a',1,2,'c','d','e',3,'s')
+f={1,2,3,4,'f','g','h'}
+g=[1,'a',2,'b',3,'c',4,'d']
+h=('a:1','b=b','c:3','d:497')
+i=3+4j
+
+print('The value of a is = ',a,'and the type of a is ',type(a))
+print('The value of bis = ',b,'and the type of b is ',type(b))
+print('The value of cis = ',c,'and the type of c is ',type(c))
+print('The value of dis = ',d,'and the type of d is ',type(d))
+print('The value of eis = ',e,'and the type of e is ',type(e))
+print('The value of fis = ',f,'and the type of f is ',type(f))
+print('The value of gis = ',g,'and the type of g is ',type(g))
+print('The value of his = ',h,'and the type of h is ',type(h))
+print('The value of iis = ',i,'and the type of i is ',type(i))

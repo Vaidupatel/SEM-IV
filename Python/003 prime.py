4f0eb2e495533225cafcf18b63fe3b82c068de7b
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2023-01-11 20:12:47 +0530

    Create 003 prime.py

---
+++ Python/003 prime.py
@@ -0,0 +1,16 @@
+def f1(x):
+     Flag=False
+     for i in range(2,x):
+          if x%i==0:
+               Flag=True
+               break
+     if Flag:
+          print('The',x,'is prime')
+     else:
+          print('The',x,'is not prime')
+for j in range(100,201):
+     x=j
+     f1(x)
+
+
+

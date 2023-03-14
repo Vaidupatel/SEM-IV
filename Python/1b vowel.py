d28d37d7833fd9f7cc19a0ef3dbf5633466a0705
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2023-03-14 18:24:18 +0530

    Update and rename 002 vowel.py to 1b vowel.py

--- Python/1b vowel.py
+++ Python/1b vowel.py
@@ -1,3 +1,5 @@
+# Write a program to check whether a passed letter is a vowel or not.
+
 x=input('Enter the character to chack wther it\'s vovel or not ')
 if(x=='a' or x=='A' or x=='e' or x=='E'or x=='i' or x=='I' or x=='o' or x=='O' or x=='u' or x=='U'):
     print('The character is vowel ( ͡° ͜ʖ ͡°) ')
